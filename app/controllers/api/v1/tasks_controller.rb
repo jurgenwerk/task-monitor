f6a4b33838d8e53e836377c094c3f445a4177f31
Matic Jurglic <matic@jurglic.si>
2017-06-25 22:44:56 +0200

    Add date param to tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -8,7 +8,14 @@ module Api
           current_account_user.app_monitors.find(params[:filter][:app_monitor_id])
 
         # TODO: add date param
-        tasks = app_monitor.tasks
+        date = Date.parse(params[:filter][:date])
+        tasks =
+          app_monitor
+          .tasks
+          .joins(:task_instances)
+          .where("task_instances.created_at >= ?", date.beginning_of_day)
+          .where("task_instances.created_at <= ?", date.end_of_day)
+
         render json: tasks
       end
     end
